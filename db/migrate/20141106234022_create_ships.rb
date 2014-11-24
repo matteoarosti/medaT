e9dd8ae650ff3455fe72278c36b5a69fee7aff0f
enricodetti <enrico.detti@alice.it>
2014-11-24 14:46:14 -0800

    Correzione model Ship

--- db/migrate/20141106234022_create_ships.rb
+++ db/migrate/20141106234022_create_ships.rb
@@ -2,8 +2,8 @@ class CreateShips < ActiveRecord::Migration
   def change
     create_table :ships do |t|
       t.string      :name,                  :limit => 50
-      t.string      :shot_name,             :limit => 4
-      t.string      :call_sign,             :limit => 4
+      t.string      :short_name,             :limit => 4
+      t.string      :call_sign,             :limit => 10
       ##t.integer     :shipowner_id,          :limit => 4
       t.belongs_to :shipowner
       t.timestamps

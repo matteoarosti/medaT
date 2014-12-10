f520d4f71c69d85f0c872e71c253fbb80c721bc6
enricodetti <enrico.detti@alice.it>
2014-12-09 16:59:50 -0800

    Traduzione del file YAML e delle Views in Terminal_Movs

--- db/migrate/20141110215337_create_handling_items.rb
+++ db/migrate/20141110215337_create_handling_items.rb
@@ -4,8 +4,8 @@ class CreateHandlingItems < ActiveRecord::Migration
       t.integer     :handling_header_id,    :limit => 8
       t.datetime    :date
       t.string      :handling_item_type,    :limit => 10  #es: SBARCO, IMBARCO, VISITA DOGANALE, ...
-      #t.integer     :handling_type,         :limit => 1 ??? Cosa rappresenta?
-      #t.integer     :container_status,      :limit => 1 ??? Cosa rappresenta?
+      t.integer     :handling_type,         :limit => 1 #IN or OUT
+      t.integer     :container_status,      :limit => 1 #FULL or EMPTY
       t.integer     :ship_id,               :limit => 8
       t.string      :voyage,                :limit => 15
       t.integer     :carrier_id,            :limit => 8
@@ -17,8 +17,6 @@ class CreateHandlingItems < ActiveRecord::Migration
       t.timestamps
 
       #Matteo
-      t.string      :eu,                    :limit => 1
-      t.string      :pv,                    :limit => 1
       t.integer     :booking_id
     end
   end

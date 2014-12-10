e4ca8fe2ca772d792efd54b8fb73435b1fedeb2c
Matteo Arosti <matteoarosti@MacBook-Pro-di-Matteo-Arosti.local>
2014-12-10 03:08:34 +0100

    ricerca e modifica booking (inizio)

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,5 +8,27 @@ class BookingsController < ApplicationController
   @item = Booking.new
   render :partial => 'form'
  end
+ 
+ def form_edit
+  @item = Booking.find(params[:rec_id])
+  render :partial => 'form'
+ end
+
+
+ 
+ def form_search
+ end
+ 
+ 
+ 
+#############################################################
+def search_num_booking
+#############################################################
+  ret = {}
+   ret[:items] = Booking.like_num_booking(params[:num_booking]).limit(1000).as_json(Booking.as_json_prop)
+   ret[:success] = true
+   render json: ret
+end
+ 
 
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,23 @@
 class Booking < ActiveRecord::Base
+
+ belongs_to :shipowner
+ belongs_to :equipment
+
+ scope :like_num_booking, ->(num_booking) {where("num_booking LIKE ?", "%#{num_booking}%")}
+ 
+ def shipowner_id_Name
+  self.shipowner.name if self.shipowner
+ end
+ 
+ def equipment_id_Name
+  self.equipment.sizetype if self.equipment
+ end
+ 
+  def self.as_json_prop()
+     return {
+        :include=>{:shipowner => {:only=>[:name]}, :equipment => {:only=>[:size]}},
+        :methods => [:shipowner_id_Name, :equipment_id_Name]
+      }
+ end
+ 
 end

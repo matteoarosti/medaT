9ab7ce80a831ba85f5afa87a67841986aaac9811
Matteo Arosti <matteo.arosti@gmail.com>
2015-01-20 00:31:07 +0100

    Equipment: gestione flag frigo

--- db/migrate/20141106234541_create_equipment.rb
+++ db/migrate/20141106234541_create_equipment.rb
@@ -5,6 +5,7 @@ class CreateEquipment < ActiveRecord::Migration
       t.integer     :size,                  :limit => 2
       t.string      :sizetype,              :limit => 50
       t.string      :iso,                   :limit => 4
+      t.boolean     :reefer,   :default => false
       t.timestamps
     end
   end

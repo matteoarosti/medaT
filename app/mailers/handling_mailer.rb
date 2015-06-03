4935606967a76859bfbb952bce0d08012d1b80a2
Matteo Arosti <matteo.arosti@gmail.com>
2015-06-04 01:02:56 +0200

    bug

--- app/mailers/handling_mailer.rb
+++ app/mailers/handling_mailer.rb
@@ -1,11 +1,11 @@
 class HandlingMailer < ActionMailer::Base
-  default from:  Rails.application.organization.sched_default_email
+  default from: "terminal@icopsrl.net"
   
   
   #in fase di creazione (on in fase di effettiva movimentazione)
   #invio email a vettore (carrier)
   def hh_on_create_to_carrier_email(hi)
     @hi = hi    
-    mail(:to => "matteo.arosti@gmail.com", :subject => "Notifica automatica2")
+    mail(:to => "matteo.arosti@gmail.com", :subject => "Notifica automatica")
   end
 end

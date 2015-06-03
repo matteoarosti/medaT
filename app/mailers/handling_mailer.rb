7c7ef8e448b94c0a0b5a56e4cd5cccca44506b92
Matteo Arosti <matteo.arosti@gmail.com>
2015-06-04 00:53:02 +0200

    organizatio.yml per sched_default_email

--- app/mailers/handling_mailer.rb
+++ app/mailers/handling_mailer.rb
@@ -1,11 +1,11 @@
 class HandlingMailer < ActionMailer::Base
-  #default from: "matteo.arosti@gmail.com"
+  default from:  Rails.application.organization.sched_default_email
   
   
   #in fase di creazione (on in fase di effettiva movimentazione)
   #invio email a vettore (carrier)
   def hh_on_create_to_carrier_email(hi)
     @hi = hi    
-    mail(:from => "matteo.arosti@gmail.com", :to => "matteo.arosti@gmail.com", :subject => "Notifica automatica")
+    mail(:to => "matteo.arosti@gmail.com", :subject => "Notifica automatica2")
   end
 end

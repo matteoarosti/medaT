b752376a04ce936624731543b80523e2d88323c8
Matteo Arosti <matteo.arosti@gmail.com>
2015-06-17 15:56:02 +0200

    stampe in terminal

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,12 +5,26 @@ class ReportsController < ApplicationController
   @hh = HandlingHeader.new
   @hi = HandlingItem.new
  end    
-  
+
+ def in_terminal_parameters
+  @hh = HandlingHeader.new
+  @hi = HandlingItem.new
+ end    
+
+ 
+   
  def handlings_sint
  end  
  
  def handlings_analytic
  end  
  
+
+  def in_terminal_sint
+  end  
   
+  def in_terminal_analytic
+  end  
+ 
+   
 end

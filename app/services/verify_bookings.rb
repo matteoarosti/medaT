0955f22eb953566e9b3c9af2a988f2e438ed3f53
Matteo Arosti <matteo.arosti@gmail.com>
2015-07-02 16:30:35 +0200

    modifica service VerifyBooking

--- app/services/verify_bookings.rb
+++ app/services/verify_bookings.rb
@@ -3,16 +3,12 @@ class VerifyBookings
     #recupero tutti gli hi di cui ancora devo inviare la email (se necessario)
     Booking.where('status=?', 'OPEN').each do |bh|
 
-      print "\n----------------------------\n"
-      print "\nBooking #{bh.id.to_s} - #{bh.num_booking.to_s}"
-
       to_close = true
       
       #verifico se tutti gli ii sono chiusi
       bh.booking_items.each do |bi|
        to_close = false if bi.status == 'OPEN'
       end
-
 
       if to_close == true
         print "\n-->Chiudo booking: #{bh.num_booking}"

926d47b42a77162a3558dcf220496bd2775aaa38
Matteo Arosti <matteo.arosti@gmail.com>
2018-01-19 01:26:20 +0100

    booking calendar

--- app/controllers/booking_calendar_controller.rb
+++ app/controllers/booking_calendar_controller.rb
@@ -8,7 +8,6 @@ class BookingCalendarController < ApplicationController
   def booking_date_items
     @items = Booking.where('1=1')
     @items = @items.where(eta: params[:date])
-    logger.info @items.to_yaml
     #render json: items.as_json(Booking.as_json_prop)
   end
   

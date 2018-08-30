cc968c53dfe8cb5f955c4db540076c30bb95fb8c
Matteo Arosti <matteo.arosti@gmail.com>
2018-08-31 00:50:12 +0200

    test

--- app/controllers/syncro_from_meda_t.rb
+++ app/controllers/syncro_from_meda_t.rb
@@ -1,5 +1,7 @@
 class SyncroFromMedaT < ApplicationController
   
+  skip_before_action :authenticate_user!
+  
   def test
     render json: {:success => true, :data=>{aaaaaa: 3333, params: params}}
   end   

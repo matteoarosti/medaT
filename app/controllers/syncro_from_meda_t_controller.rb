a5364cf5cfb12d724ffa25314bafa94eb1aa02ae
Matteo Arosti <matteo.arosti@gmail.com>
2018-08-31 01:05:33 +0200

    SyncroFromMedaT

--- app/controllers/syncro_from_meda_t_controller.rb
+++ app/controllers/syncro_from_meda_t_controller.rb
@@ -1,9 +1,13 @@
 class SyncroFromMedaTController < ApplicationController
   
+  #al momento disattivo controllo utente e CSRF session
+  protect_from_forgery with: :null_session
   skip_before_action :authenticate_user!
   
-  def test
-    render json: {:success => true, :data=>{aaaaaa: 3333, params: params}}
+  def get_discharge_hi_for_container
+    ship = Ship.find_by_name(params[:ship_name])
+    hi = HandlingItem.where(container_number: params[:container_number], ship_id: ship.id, voyage: params[:voyage])
+    render json: {:success => true, :data=>{hi: hi}}
   end   
   
 end

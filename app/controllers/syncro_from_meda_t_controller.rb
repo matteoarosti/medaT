5f3a0f230c7fb100a87144534948ec166f697f2d
Matteo Arosti <matteo.arosti@gmail.com>
2018-09-03 16:17:11 +0200

    syncro from medaT (imo code)

--- app/controllers/syncro_from_meda_t_controller.rb
+++ app/controllers/syncro_from_meda_t_controller.rb
@@ -5,9 +5,14 @@ class SyncroFromMedaTController < ApplicationController
   skip_before_action :authenticate_user!
   
   def get_discharge_hi_for_container
-    ship = Ship.find_by_name(params[:ship_name])
-    hi = HandlingItem.where(container_number: params[:container_number], ship_id: ship.id, voyage: params[:voyage])
-    render json: {:success => true, :data=>{hi: hi}}
+    ship = Ship.find_by_imo_code(params[:ship_imo_code])
+    if ship.nil?
+      raise "Nave non trovata con IMO #{params[:ship_imo_code]}"
+    end
+    hi = HandlingItem.joins(:handling_header).where(handling_headers: {container_number: params[:container_number]}, ship_id: ship.id, voyage: params[:voyage]).first
+    #hi = HandlingItem.joins(:handling_header).where(handling_headers: {container_number: params[:container_number]}, ship_id: ship.id).first
+    ret_movs = hi.handling_header.handling_items.order(:datetime_op).select {|r| ['I_DISCHARGE', 'O_EMPTYING'].include?(r.handling_item_type)}
+    render json: {:success => true, :data=>{hi: ret_movs}}
   end   
   
 end

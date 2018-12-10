9042f0a6a1858d2a29ea877a2e3e7b0068648296
Matteo Arosti <matteo.arosti@gmail.com>
2018-12-11 00:37:49 +0100

    nuova gestione visite doganali

--- app/models/activity_op.rb
+++ app/models/activity_op.rb
@@ -2,4 +2,10 @@ class ActivityOp < ActiveRecord::Base
   belongs_to :activity_type
   
   scope :extjs_default_scope, -> {}
+    
+    
+  def self.all_by_type_code(type_code)
+    at = ActivityType.find_by_code(type_code)
+    return at.activity_ops
+  end
 end
--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -1,3 +1,4 @@
 class ActivityType < ActiveRecord::Base
+  has_many :activity_ops
   scope :extjs_default_scope, -> {}
 end

b1cb5dc630ec0d61fee6dceda55920be6e0fba71
Matteo Arosti <matteo.arosti@gmail.com>
2019-02-18 00:26:31 +0100

    send email from log event

--- app/services/send_email_from_log_event.rb
+++ app/services/send_email_from_log_event.rb
@@ -3,12 +3,26 @@ class SendEmailFromLogEvent
     
     LogEvent.where(operation_crud: 'E').where(result_at: nil).each do |i|
       ij = (ActiveSupport::JSON.decode i.notes).with_indifferent_access
+      
+      
+      
+      begin
+        HandlingMailer.send_simple(ij[:email][:to], 
+                                   ij[:email][:subject], 
+                                   ij[:email][:msg],
+                                   ij[:email][:content_type] || 'text/plain').deliver!
+        ij.result_at = Time.zone.now                             
+        ij.save!
+      rescue Exception => e
+        #memorizzo l'errore
+        ij.result_at = Time.zone.new
+        ij.result_notes = e.message
+        ij.save!
+      end
+      
+      
             
-      ret = HandlingMailer.send_simple(ij[:email][:to], 
-                                 ij[:email][:subject], 
-                                 ij[:email][:msg],
-                                 ij[:email][:content_type] || 'text/plain').deliver!
-      puts ret.to_json
+      
     end     
     
   end #call

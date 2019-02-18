bd8a64d2fee1e6566b20f47fab7684549df9880e
Matteo Arosti <matteo.arosti@gmail.com>
2019-02-18 01:19:52 +0100

    activity_dett list all add execution/confirmed user name

--- app/models/activity_dett_container.rb
+++ app/models/activity_dett_container.rb
@@ -21,12 +21,28 @@ class ActivityDettContainer < ActiveRecord::Base
               include: {activity_op: {}, shipowner: {}, customer: {only: 'name'}},
               :methods => [:created_user_name]
            }
-         }
+         },
+         :methods => [:confirmed_user_name, :execution_user_name]
        }
   end     
   
   def self.default_scope
     Activity::default_scope
   end
+  
+  
+  def execution_user_name
+    u = User.find(self.execution_user_id) rescue u = nil
+    return u.name if !u.nil?
+    self.execution_user_id
+  end
+  
+  
+ def confirmed_user_name
+   u = User.find(self.confirmed_user_id) rescue u = nil
+   return u.name if !u.nil?
+   self.confirmed_user_id
+ end
+  
     
 end

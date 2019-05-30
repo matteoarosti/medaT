c9f7037e50decc61b1c85f8832dd1282e157b01a
Matteo Arosti <matteo.arosti@gmail.com>
2019-05-31 00:47:20 +0200

    bug

--- db/migrate/20190531004328_alter_customers_add_email_weigh.rb
+++ db/migrate/20190531004328_alter_customers_add_email_weigh.rb
@@ -1,5 +1,5 @@
 class AlterCustomersAddEmailWeigh < ActiveRecord::Migration
   def change
-    dd_column   :customers,  :email_notify_weigh, :string, limit: 300
+    add_column   :customers,  :email_notify_weigh, :string, limit: 300
   end
 end

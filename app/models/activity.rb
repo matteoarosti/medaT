0bdbb07363cd4ec384ec8f92a338b916a70c494b
Matteo Arosti <matteo.arosti@gmail.com>
2019-11-26 23:26:38 +0100

    test

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -28,7 +28,7 @@ class Activity < ActiveRecord::Base
            :activity_op => {:only=>[:name, :default_price, :recalculate_gest_price]},
            :activity_dett_containers => {
                 :only => [:container_number, :status, :make_available_at, :execution_at, :confirmed_at],
-                :methods => [:dra_out]}
+                :methods => [:dra_out]
            }
            },
          :methods => [:created_user_name]

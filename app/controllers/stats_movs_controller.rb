1423c1d428a965bb2761bf89144ae673f65ae862
matteoarosti <root@srv-web.localdomain>
2020-12-17 00:46:38 +0100

    statistiche in/out container

--- app/controllers/stats_movs_controller.rb
+++ app/controllers/stats_movs_controller.rb
@@ -7,10 +7,21 @@ class StatsMovsController < ApplicationController
   end
     
      
+
+  def graph_date_movs_by_carrier
+	graph_data_movs_grouping('CARRIER')
+  end
   
-  
+
   def graph_data_movs_by_hours
+	graph_data_movs_grouping
+  end
+
+  
+  def graph_data_movs_grouping(grouping = 'HOURS')
+	###logger.info Time.zone.formatted_offset
     ret = {}
+	ar_op = {}
     ret[:items] = []
     r = {'IE' => {}, 'OE' => {}, 'LE' => {}, 'DE' => {}, 'IF' => {}, 'OF' => {}, 'LF' => {}, 'DF' => {}}
 
@@ -23,30 +34,49 @@ class StatsMovsController < ApplicationController
 =end
 
     where_by_form_values = ' '
-        
+	where_by_form_values += " AND carrier_id IS NOT NULL"
+
+	if grouping == 'CARRIER'
+			group_by_field = " carrier_id "			
+	else
+		group_by_field = " hour(datetime_op) "
+	end
+
     unless params[:formValues].nil?  
       where_by_form_values += " AND datetime_op >= '#{Time.zone.parse(params[:formValues]['flt_date_from']).beginning_of_day}'"  unless params[:formValues]['flt_date_from'].blank?
       where_by_form_values += " AND datetime_op <= '#{Time.zone.parse(params[:formValues]['flt_date_to']).end_of_day}'"  unless params[:formValues]['flt_date_to'].blank?        
     end
       
+
+
     gcs = HandlingHeader.find_by_sql("
       SELECT
         dd.op_h, dd.handling_type, dd.handling_item_type, dd.container_FE, sum(dd.t_cont) as t_cont
       FROM (        
-          SELECT date(datetime_op) as op_dd, hour(datetime_op) as op_h, hi.handling_type, hi.handling_item_type, hi.container_FE, 
+          SELECT 
+			#{group_by_field} as op_h, 
+			hi.handling_type, hi.handling_item_type, hi.container_FE, 
            count(*) as t_cont 
           FROM handling_headers hh 
           INNER JOIN handling_items hi ON hh.id = hi.handling_header_id
-          WHERE (hi.operation_type='MT' and hi.container_FE <>'')
+		  WHERE (hi.operation_type='MT' and hi.container_FE <>'')
             #{where_by_form_values}
-          GROUP BY date(datetime_op), hour(datetime_op), hi.handling_type, hi.handling_item_type, container_FE
+          GROUP BY #{group_by_field}, 
+		  hi.handling_type, hi.handling_item_type, container_FE
       ) as dd
       GROUP BY dd.op_h, dd.handling_type, dd.handling_item_type, container_FE
     ")
      
-     
+
      
      gcs.each do |gc|
+
+	   #correggo timezone
+	   if grouping == 'HOURS'
+		   tt = Time.new(1, 1, 1, gc.op_h, 0, 00, "+00:00")		
+		   gc.op_h = tt.localtime.hour	
+	   end
+
        if gc.handling_item_type == 'O_LOAD'
          r["L#{gc.container_FE}"]["#{gc.op_h}"] = r["L#{gc.container_FE}"]["#{gc.op_h}"].to_i + gc.t_cont;
        elsif gc.handling_item_type == 'I_DISCHARGE'
@@ -54,34 +84,45 @@ class StatsMovsController < ApplicationController
        else
         r["#{gc.handling_type}#{gc.container_FE}"]["#{gc.op_h}"] = r["#{gc.handling_type}#{gc.container_FE}"]["#{gc.op_h}"].to_i + gc.t_cont;
        end
+
+	   ar_op["#{gc.op_h}"] = 1
+
      end
 
  
-     
-    #array per le 24 ore          
-     24.times do |hm|
-       
-       #ToDo: devo aggiungere +2 alle ore????? (timezone)
-       if hm >= 2 
-         h = hm - 2
-       else
-         h = hm + 22
-       end
-       #h = hm
-       
-       s = {}
-       s[:op] = hm
-       s[:IE]  = r["IE"]["#{h}"] || 0
-       s[:OE]  = r["OE"]["#{h}"] || 0
-       #s[:LE]  = r["LE"]["#{h}"] || 0
-       #s[:DE]  = r["DE"]["#{h}"] || 0
-       s[:IF]  = r["IF"]["#{h}"] || 0
-       s[:OF]  = r["OF"]["#{h}"] || 0
-       #s[:LF]  = r["LF"]["#{h}"] || 0
-       #s[:DF]  = r["DF"]["#{h}"] || 0         
-       ret[:items] << s       
-     end
-       
+
+	if grouping == 'HOURS'     
+	    #array per le 24 ore          
+	     24.times do |h|
+	       s = {}
+	       s[:op] = h
+	       s[:IE]  = r["IE"]["#{h}"] || 0
+	       s[:OE]  = r["OE"]["#{h}"] || 0
+	       #s[:LE]  = r["LE"]["#{h}"] || 0
+	       #s[:DE]  = r["DE"]["#{h}"] || 0
+	       s[:IF]  = r["IF"]["#{h}"] || 0
+	       s[:OF]  = r["OF"]["#{h}"] || 0
+	       #s[:LF]  = r["LF"]["#{h}"] || 0
+	       #s[:DF]  = r["DF"]["#{h}"] || 0         
+	       ret[:items] << s       
+	     end
+	else
+	
+		ar_op.each do |h, v|
+		   s = {}
+	       s[:op] = Carrier.find(h).name[0, 10]
+		   
+	       s[:IE]  = r["IE"]["#{h}"] || 0
+	       s[:OE]  = r["OE"]["#{h}"] || 0
+	       s[:IF]  = r["IF"]["#{h}"] || 0
+	       s[:OF]  = r["OF"]["#{h}"] || 0
+		   s[:global_sum] = s[:IE] + s[:OE] + s[:IF] + s[:OF]
+	       ret[:items] << s
+		end
+		ret[:items] = ret[:items].sort_by { |k| -k[:global_sum] }
+	end
+
+
     render json: ret
   end
   

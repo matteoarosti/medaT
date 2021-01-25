d02031c4de660a170227b22c9a6d7f35c5c264a2
matteoarosti <root@srv-web.localdomain>
2021-01-25 23:31:02 +0100

    activity: flag non_usare

--- app/controllers/activity_ops_controller.rb
+++ app/controllers/activity_ops_controller.rb
@@ -4,4 +4,21 @@ class ActivityOpsController < ApplicationController
    'ActivityOp'
   end   
   
+  
+  def get_combo_data_escludi_non_usare
+    ret = {}
+    model_class = 'ActivityOp'
+    
+    m_where = "non_usare = false"
+    if params[:id_selected]
+      m_where += " or id = #{params[:id_selected]}"
+    end
+    
+    ret[:success] = true
+    ret[:items] = model_class.constantize
+         .where(m_where)
+         .order(model_class.constantize.combo_displayField()).limit(500)
+    render json: ret   
+  end
+  
 end
